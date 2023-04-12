91533608d4ae7c37961a0aca85659f4ed40d5a80
lumeyisnotyou <lumey@lumey.dev>
2023-04-12 19:47:40 -0400

    Initial Commit

---
+++ BudBud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudBud
+//
+//  Created by Miles Axelberd on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
